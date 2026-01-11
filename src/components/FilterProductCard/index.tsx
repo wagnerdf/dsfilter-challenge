f12a7fb2858faf0e4187b9e17a2881a2af8aa152
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2026-01-11 10:54:23 -0300

    Handling numeric fields

--- src/components/FilterProductCard/index.tsx
+++ src/components/FilterProductCard/index.tsx
@@ -1,28 +1,53 @@
+import { useState } from "react";
 import "./styles.css";
 
+type FormData = {
+  numberMin?: number;
+  numberMax?: number;
+};
+
 export default function FilterProductCard() {
+  const [formData, setFormData] = useState<FormData>({
+    numberMin: undefined,
+    numberMax: undefined,
+  });
+
+  function handleInputChange(event: any) {
+    const value = event.target.value;
+    const name = event.target.name;
+    setFormData({ ...formData, [name]: value });
+  }
+
+  function handleSubmit(event: any){
+    event.preventDefault();
+    console.log(formData.numberMin || 0);
+    console.log(formData.numberMax || Number.MAX_VALUE);
+  }
+
   return (
     <div className="container">
       <div className="dsf-container-card-form">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <input
-              id="dsf-inpt"
-              name="min"
+              name="numberMin"
+              value={formData.numberMin || ""}
               type="text"
               placeholder="Preço mínimo"
+              onChange={handleInputChange}
             />
           </div>
           <div>
             <input
-              id="dsf-inpt"
-              name="max"
+              name="numberMax"
+              value={formData.numberMax || ""}
               type="text"
               placeholder="Preço máximo"
+              onChange={handleInputChange}
             />
           </div>
           <div>
-            <button className="dsf-btn">Filtrar</button>
+            <button>Filtrar</button>
           </div>
         </form>
       </div>
